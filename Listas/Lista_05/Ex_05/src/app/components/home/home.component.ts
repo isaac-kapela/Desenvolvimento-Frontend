43c91ae03e5c19c9c3fe81cde7e9bbd404dff55e
isaac-kapela <kapelajoao4@gmail.com>
2023-07-17 21:31:14 -0300

    Lista 05

---
+++ Listas/Lista_05/Ex_05/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { PokemonService } from './../../service/pokemon.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  constructor(private PokemonService: PokemonService){ }
+
+}
